b3b63cfbecd95eea730cee2e45482c0b15453a6e
Michael Montero <mike@crowdtwist.com>
2012-01-30 22:24:29 -0500

    Increase cURL timeout values even higher to deal with larger file sizes.

--- src/v1/CrowdTwist.php
+++ src/v1/CrowdTwist.php
@@ -424,10 +424,10 @@ extends _crwd_Api
                     true);
         curl_setopt($curl_handle,
                     CURLOPT_CONNECTTIMEOUT,
-                    3600);
+                    10800);
         curl_setopt($curl_handle,
                     CURLOPT_TIMEOUT,
-                    3600);
+                    10800);
         curl_setopt($curl_handle,
                     CURLOPT_POST,
                     true);
@@ -466,10 +466,10 @@ extends _crwd_Api
                     true);
         curl_setopt($curl_handle,
                     CURLOPT_CONNECTTIMEOUT,
-                    3600);
+                    10800);
         curl_setopt($curl_handle,
                     CURLOPT_TIMEOUT,
-                    3600);
+                    10800);
         curl_setopt($curl_handle,
                     CURLOPT_POST,
                     true);
